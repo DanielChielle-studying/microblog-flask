9737f701a0c3a46ea6e282da340b4e40b1f7efe8
Daniel Chielle <danielchielle.studying@gmail.com>
2024-09-24 08:52:56 -0300

    Create initial HTML templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Daniel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Foz do Iguaçu'
+        },
+        {
+            'author': {'username': 'Gabriel'},
+            'body': 'I really like Flask'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
